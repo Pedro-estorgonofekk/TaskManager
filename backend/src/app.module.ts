3f09c2a6e8b374356eba55f4bd03c90fe865da24
unknown <pedroalvarenga173@gmail.com>
2026-01-19 21:04:36 -0300

    refactor: remove arquivos arquivos inuteis

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,12 @@
 //aqui é onde todos os módulos se encontra, olha eles aqui em baixo⬇️
 
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TasksService } from './tasks/tasks.service';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [TasksModule],
   controllers: [AppController],
-  providers: [AppService, TasksService],
+  providers: [TasksService],
 })
 export class AppModule {}
